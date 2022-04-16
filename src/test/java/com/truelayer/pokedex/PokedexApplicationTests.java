d4ba17353683da6765d56edce069e037f5205a10
Ajay Singh <>
2022-04-16 13:23:05 +0530

    Initial backend spring boot skeleton

---
+++ src/test/java/com/truelayer/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,11 @@
+package com.truelayer.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
